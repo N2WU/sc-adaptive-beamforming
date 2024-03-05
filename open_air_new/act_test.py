da61dfba1c15b4d0a11ae18febdb3125e31fec02
Nolan Pearce <pearcenolan@gmail.com>
2024-03-05 12:34:25 -0500

    add new open-air files

---
+++ open_air_new/act_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sounddevice as sd   
+
+fs = 96000
+duration = 5
+channels = 8
+
+output_channels = range(3)
+input_channels = range(channels)
+
+sd.default.device = "ASIO MADIface USB"
+sd.default.channels = channels
+sd.default.samplerate = fs
+
+t = np.linspace(0, duration, num=duration*fs)
+y = np.cos(2 * np.pi * 10000 * t)
+tx = np.zeros((len(t), channels))
+
+for ch in output_channels:
+    tx[:, ch] = y
+
+rx = np.squeeze(sd.playrec(tx * 0.1, blocking=True))
